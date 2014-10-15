dcb0c134abc22392ceb64fdf05a5b45c6910a66c
mxro <mxro@nowhere.com>
2014-10-15 11:52:44 +1100

    Assembling http-server implementation for Netty 4 Autocommit Wed Oct 15 11:52:44 EST 2014

--- src/main/java/de/mxro/httpserver/netty4/ByteStreamHandler.java
+++ src/main/java/de/mxro/httpserver/netty4/ByteStreamHandler.java
@@ -5,10 +5,12 @@
  ******************************************************************************/
 package de.mxro.httpserver.netty4;
 
+import io.netty.handler.codec.http.HttpContent;
+
 import java.io.ByteArrayOutputStream;
 
 public interface ByteStreamHandler {
 
-    public void processRequest(ByteArrayOutputStream receivedData, MessageEvent e);
+    public void processRequest(ByteArrayOutputStream receivedData, HttpContent e);
 
 }
